cd5f6fb12ef3ae5d7a0f028de0f02f650a149dea
Wyat Hedge <WyatHedge@outlook.com>
2019-09-12 15:21:13 -0400

    Section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import {FormsModule} from '@angular/forms';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 export class CustomHammerConfig extends HammerGestureConfig {
    overrides = {
@@ -15,14 +19,18 @@ export class CustomHammerConfig extends HammerGestureConfig {
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [
-      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+      AuthService
+      //provide
    ],
    bootstrap: [
       AppComponent
